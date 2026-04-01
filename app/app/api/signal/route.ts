22958ba45ee5ea1d94b309b8458fb765230a0d6d
Lorenzo <lo9791@hotmail.com>
2026-04-01 17:39:50 +0100

    add signal route

--- app/app/api/signal/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-  const res = await fetch("http://127.0.0.1:8000/signal", {
-    cache: "no-store",
-  });
-
-  const data = await res.json();
-  return Response.json(data);
-}
