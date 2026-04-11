1899b7e32f81c5a41994da0f17a3458b08252e27
Lorenzo <lo9791@hotmail.com>
2026-04-11 10:41:11 +0100

    final stripe api fix

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -4,7 +4,7 @@ import Stripe from "stripe"
 import { NextResponse } from "next/server"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-03-31.dahlia",
+  apiVersion: "2026-03-25.dahlia",
 })
 
 function getPaidUsersPath() {
