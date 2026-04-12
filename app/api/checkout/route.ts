8a2500fbfd83b52d5a60f817702c372f62ef90e7
Lorenzo <lo9791@hotmail.com>
2026-04-12 07:51:22 +0100

    fix stripe api version

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -2,9 +2,7 @@ import { auth } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
 import Stripe from "stripe"
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-06-20",
-})
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export async function POST(req: Request) {
   try {
--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -2,9 +2,7 @@ import Stripe from "stripe"
 import { NextResponse } from "next/server"
 import { clerkClient } from "@clerk/nextjs/server"
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-06-20",
-})
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export async function POST(req: Request) {
   const signature = req.headers.get("stripe-signature")
